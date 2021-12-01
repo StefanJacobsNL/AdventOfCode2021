57c044804e5b07c578cb5ed43b8aba0ee2036961
Stefan Jacobs <stefanjacobsrips@outlook.com>
2021-12-01 17:37:51 +0100

    Day 01 Part

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -1,5 +1,7 @@
-increaseCounter = 0
+increaseCounterPart1 = 0
+increaseCounterPart2 = 0
 previousInput = 0
+loopCounter = 0
 
 # read file and convert to string list
 with open('input.txt') as f:
@@ -8,10 +10,33 @@ with open('input.txt') as f:
 # convert string list to int list
 content_list = list(map(int, content_list))
 
+# gets the total amount
+amountList = len(content_list)
+
+# part 1
 for item in content_list:
 	if previousInput != 0:
 		if item > previousInput:
-			increaseCounter += 1
+			increaseCounterPart1 += 1
 	previousInput = item
 
-print(increaseCounter)
+previousInput = 0
+
+# part 2
+for item in content_list:
+	if (loopCounter + 2) < amountList:
+
+		# create the total value
+		currentTotal = item + content_list[loopCounter + 1] + content_list[loopCounter + 2]
+		
+		if previousInput != 0:
+			if currentTotal > previousInput:
+				increaseCounterPart2 += 1
+
+		# set previous input
+		previousInput = currentTotal
+	
+	loopCounter += 1
+
+print("The answer is of part one is  " + str(increaseCounterPart1))
+print("The answer is of part two is  " + str(increaseCounterPart2))
