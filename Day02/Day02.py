4751ac26660998c9049d70ab6128e7648696ee3a
Stefan Jacobs <stefanjacobsrips@outlook.com>
2021-12-02 12:42:45 +0100

    Day 02 Part 2

--- Day02/Day02.py
+++ Day02/Day02.py
@@ -2,6 +2,7 @@ answerPart1 = 0
 answerPart2 = 0
 horizontal = 0
 vertical = 0
+aim = 0
 
 # read file and convert to string list
 with open('input.txt') as f:
@@ -22,4 +23,25 @@ for item in content_list:
 answerPart1 = horizontal * vertical
 
 print("The answer is of part one is  " + str(answerPart1))
+
+# part 2
+horizontal = 0
+vertical = 0
+
+for item in content_list:
+    newWay = item.split(" ")[0]
+    newAmount = int(item.split(" ")[1])
+    
+    if newWay == "forward":
+        horizontal += newAmount
+
+        if aim > 0:
+            vertical += newAmount * aim
+    elif newWay == "down":
+        aim += newAmount
+    elif newWay == "up":
+        aim -= newAmount
+
+answerPart2 = horizontal * vertical
+
 print("The answer is of part two is  " + str(answerPart2))
